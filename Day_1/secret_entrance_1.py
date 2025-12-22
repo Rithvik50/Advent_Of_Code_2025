5813762b62e30e6d039247c6eb4902a736b6fca8
Rithvik Muthyalapati <rmuthyalapati@gmail.com>
2025-12-22 21:23:27 +0530

    Day 1 Complete

---
+++ Day_1/secret_entrance_1.py
@@ -0,0 +1,12 @@
+with open("sample.txt", 'r') as f:
+    lines = f.readlines()
+    rotations = [line.strip() for line in lines]
+
+dial = 50
+password = 0
+for rotation in rotations:
+    dir, val = rotation[0], int(rotation[1:])
+    dial = dial + val if dir == "R" else dial - val
+    if dial % 100 == 0:
+        password += 1
+print(password)
---
+++ Day_1/secret_entrance_2.py
@@ -0,0 +1,14 @@
+with open("input.txt", 'r') as f:
+    lines = f.readlines()
+    rotations = [line.strip() for line in lines]
+
+dial = 50
+password = 0
+for rotation in rotations:
+    dir, val = rotation[0], int(rotation[1:])
+    step = 1 if dir == "R" else -1
+    for _ in range(val):
+        dial = (dial + step) % 100
+        if dial == 0:
+            password += 1
+print(password)
